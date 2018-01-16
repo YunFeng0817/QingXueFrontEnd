7f680edb5e8fb0ae83c2d2c9521a8061906d2257
Fitz <f18846188605@gmail.com>
2018-01-17 01:12:50 +0800

    add css to footer

--- src/components/footerIndex.vue
+++ src/components/footerIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="footer-index">
     <router-link to='/dynamic'>动态</router-link>
     <router-link to='/main'>主页</router-link>
     <router-link to='/user'>用户</router-link>
@@ -10,6 +10,32 @@
   export default {}
 </script>
 
-<style scoped>
+<style scoped rel="stylesheet/css">
+  .footer-index {
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
 
+    background-color: #0e90d2;
+    opacity: 0.8;
+    font-size: 16px;
+    color: white;
+  }
+
+  .footer-index a {
+    flex: 1;
+    text-align: center;
+    /*
+     *if it's block ,when click the box,link work  ,
+     *but if it's in-line ,don't work when click blank in box
+     */
+    display: block;
+    text-decoration: none;
+  }
+
+  .router-link-active{
+    color: lightgray;
+    font-size:20px;
+  }
 </style>
