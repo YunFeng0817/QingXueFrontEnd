13e758e2db32c57cc172ffb9dca720e870cf6b95
Fitz <f18846188605@gmail.com>
2018-01-19 16:47:50 +0800

    完成了头部样式的设计

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <header-index></header-index>
     <router-view></router-view>
     <footerIndex></footerIndex>
   </div>
@@ -8,10 +9,12 @@
 
 <script type="text/ecmascript-6">
   import footerIndex from './components/footerIndex'
+  import headerIndex from './components/headerIndex'
 
   export default {
     name: 'app',
     components: {
+      headerIndex,
       footerIndex
     }
   }
---
+++ src/components/headerIndex.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <a>
+        <i class="am-header-icon am-icon-map-marker"></i>
+      </a>
+      <am-topbar-form>
+        <am-form-group size="sm">
+          <am-input id="input" placement="请输入要查询的内容"></am-input>
+        </am-form-group>
+      </am-topbar-form>
+      <span class="am-input-group-btn">
+        <button class="am-btn am-btn-primary" type="button"><span class="am-icon-search"></span></button>
+      </span>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-index'
+  }
+</script>
+
+<style scoped type="text/css" rel="stylesheet">
+  div{
+    width: 100%;
+    height: 100%;
+    line-height: 50px;
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+
+    background-color: #0e90d2;
+  }
+  div a{
+    flex:10%;
+
+    text-align: center;
+    color: white;
+    font-size:25px;
+  }
+  div am-topbar-form{
+    flex:80%;
+  }
+
+  #input{
+    position: relative;
+    top:10px;
+  }
+
+  div span{
+    flex:1 10%;
+  }
+
+  div button{
+    border: none;
+  }
+</style>
