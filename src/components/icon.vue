a10beca8b6a0110ce6ebe367b7af6acb46a6e1f1
Fitz <f18846188605@gmail.com>
2018-01-20 13:26:10 +0800

    实现了添加小图标，但是样式还是有一些问题，还有几个警告未消除

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -1,6 +1,6 @@
 <template>
   <li style="text-align:center;">
-    <a class="am-icon-btn" :class="category_tag.class"></a>
+    <a class="am-icon-btn" :class="category_tag.iconType"></a>
     <span style="display: block;">
       {{category_tag.message}}
     </span>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="main">
     <!--playDelay 是自动播放的延迟时间-->
     <am-slider id="slider">
       <am-slider-item><img src="http://s.amazeui.org/media/i/demos/bing-1.jpg"/></am-slider-item>
@@ -7,36 +7,41 @@
       <am-slider-item><img src="http://s.amazeui.org/media/i/demos/bing-3.jpg"/></am-slider-item>
       <am-slider-item><img src="http://s.amazeui.org/media/i/demos/bing-4.jpg"/></am-slider-item>
     </am-slider>
-    <icon v-for='item in items' v-bind:category_tag="item" v-key="item.id"></icon>
+    <ul class="am-sm-4">
+      <icon v-for='item in items' v-bind:category_tag="item"></icon>
+    </ul>
   </div>
 </template>
 
 <script>
   import icon from './icon'
-  import Vue from 'vue'
+  // import Vue from 'vue'
 
   export default {
     name: 'Main',
     components: {
       icon: icon
+    },
+    data () {
+      return {
+        items: [
+          {message: '小学', iconType: 'am-primary am-icon-child'},
+          {message: '初中', iconType: 'am-warning am-icon-male'},
+          {message: '高中', iconType: 'am-success am-icon-fort-awesome'},
+          {message: '大学', iconType: 'am-danger am-icon-graduation-cap'},
+          {message: '考研', iconType: 'am-warning am-icon-institution'},
+          {message: '四级', iconType: 'am-danger am-icon-signal'},
+          {message: '六级', iconType: 'am-primary am-icon-rocket'},
+          {message: '艺术', iconType: 'am-warning am-icon-paint-brush'}
+        ]
+      }
     }
   }
-  /* eslint-disable no-new */
-  new Vue({
-    el: 'icon',
-    data: {
-      items: [
-        {message: '小学', class: 'am-primary am-icon-child'},
-        {message: '初中', class: 'am-warning am-icon-male'},
-        {message: '高中', class: 'am-success am-icon-fort-awesome'},
-        {message: '大学', class: 'am-danger am-icon-graduation-cap'},
-        {message: '考研', class: 'am-warning am-icon-institution'},
-        {message: '四级', class: 'am-danger am-icon-signal'},
-        {message: '六级', class: 'am-primary am-icon-rocket'},
-        {message: '艺术', class: 'am-warning am-icon-paint-brush'}
-      ]
-    }
-  });
+  // /* eslint-disable no-new */
+  // new Vue({
+  //   el: '.am-icon-btn',
+  //
+  // });
 </script>
 
 <style scoped type="text/css" rel="stylesheet">
