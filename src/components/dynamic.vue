092f9742e9910d548e1565023e8b3dd54c6d7eb3
Fitz <f18846188605@gmail.com>
2018-01-25 16:55:46 +0800

    添加了筛选框

--- src/components/dynamic.vue
+++ src/components/dynamic.vue
@@ -1,26 +1,45 @@
 <template>
   <div>
-    <list_news :typeName="typeName">
+    <div class="filter">
+      <label>选出你关注的内容</label>
+      <el-filter id="filter">
+      </el-filter>
+    </div>
+    <list_news @click.native="essay_link" :typeName="typeName">
     </list_news>
   </div>
 </template>
 
 <script>
   import listNews from './listNews'
+  import filter from './filter'
 
   export default {
     name: 'dynamic',
     components: {
-      list_news: listNews
+      list_news: listNews,
+      'el-filter': filter
     },
     data () {
       return {
         typeName: '分类的名字'
       }
+    },
+    method: {
+      essay_link () {
+        this.$router.push({path: 'article'});
+      }
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped type="text/css" rel="stylesheet">
+  .filter {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .filter label{
+    margin: 3%;
+  }
 </style>
