5058597277e4503bc2dafa39cbebc427a8cc255f
Fitz <f18846188605@gmail.com>
2018-01-31 18:40:18 +0800

    修改了用户主页的样式调整

--- src/components/user.vue
+++ src/components/user.vue
@@ -2,10 +2,10 @@
   <div>
     <div id="user-header">
       <am-image id="avatar" width="120" height="120" :circle="true" :responsive="true" :thumbnail="true"
-                :src="avatar" v-if="avatar"/>
-      <p><span class="user-message">{{userName}}</span>欢迎光临<a @click="logout">退出登录</a></p>
+                :src="avatar===''?'http://s.amazeui.org/media/i/demos/bing-4.jpg':avatar"/>
+      <p><span class="user-message">{{userName}}</span>欢迎光临<a @click="logout">退出登录</a><a style="padding: 0">修改信息</a></p>
       <p><span style="padding: 10px;">性别：</span><span class="am-btn icon" :class="getGender"
-                                                      style="font-size: large"></span> 年级：<span class="user-message">{{stage}}</span>
+                                                      style="font-size: large"></span> 年级：<span class="user-message">{{stage+grade}}</span>
       </p>
     </div>
     <el-collapse accordion id="user-collapse-body">
@@ -40,6 +40,7 @@
         userName: userMessage.state.name,
         gender: userMessage.state.gender,
         stage: userMessage.state.stage,
+        grade: userMessage.state.grade,
         panelList: [
           {
             label: ' 我的预约',
