42a0ee644709a39fb3b7a483ea08f2e05998bbca
Fitz <f18846188605@gmail.com>
2018-02-04 13:40:51 +0800

    将element-ui改为了局部应用，缩小了项目的体积

--- src/main.js
+++ src/main.js
@@ -5,12 +5,70 @@ import App from './App'
 import router from './router'
 import AmazeVue from 'amaze-vue';
 import 'amaze-vue/dist/amaze-vue.css';
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from './axios/index'
+import {
+  Autocomplete,
+  Checkbox,
+  Input,
+  Select,
+  Option,
+  Button,
+  ButtonGroup,
+  DatePicker,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Tag,
+  Icon,
+  Row,
+  Col,
+  Upload,
+  Badge,
+  Card,
+  Rate,
+  Steps,
+  Step,
+  Carousel,
+  CarouselItem,
+  Collapse,
+  CollapseItem,
+  Cascader,
+  Message
+} from 'element-ui'
+
+Vue.use(Autocomplete);
+Vue.use(Checkbox);
+Vue.use(Option);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(DatePicker);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Tag);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Upload);
+Vue.use(Badge);
+Vue.use(Card);
+Vue.use(Rate);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(Cascader);
+
+Vue.prototype.$message = Message;
 
 Vue.use(AmazeVue);
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
