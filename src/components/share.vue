4896c8eb8e94f61236457ae8ea742a6f10c5f7c6
Fitz <f18846188605@gmail.com>
2018-02-07 00:31:51 +0800

    完成了分享页面的基础样式，同时为页面添加了半透明遮罩

--- src/components/share.vue
+++ src/components/share.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div @click="shareOn">
     <transition>
-      <div class="share-row">
-        <h2>分享到</h2>
-        <hr/>
-        <icon :category_tag="items" :row="3">
-        </icon>
+      <div>
+        <div class="share-row">
+          <div class="share-header">
+            <h3>分享到</h3>
+          </div>
+          <hr/>
+          <icon :category_tag="items" :row="3">
+          </icon>
+        </div>
+        <button class="share-cancel">取消</button>
       </div>
     </transition>
   </div>
@@ -30,15 +35,51 @@
           {message: '人人网', iconType: 'am-primary am-icon-renren'}
         ]
       }
+    },
+    methods: {
+      shareOn (event) {
+        let node = event.target;
+        while (node.tagName !== 'BODY') {
+          node = node.parentNode;
+        }
+        let cover = document.createElement('div');
+        cover.style = 'width:100%;height:100%;background-color:rgba(0,0,0,0.6);position:fixed;left:0;top:0;';
+        node.appendChild(cover);
+      }
     }
   }
 </script>
 
 <style scoped>
-  div.share-row{
-    width: 100%;
-    padding: 0 5%;
+  .share-row {
+    position: relative;
     z-index: 1100;
+    margin: 0 5%;
+    background-color: white;
+    border-radius: 10px;
     text-align: center;
+  }
+
+  hr {
+    margin: 2% 0;
+  }
+
+  h3 {
+    padding: 3% 0 0 0;
+    margin: 0;
+    color: gray;
+  }
+
+  .share-cancel {
+    position: relative;
+    z-index: 1100;
+    width: 90%;
+    height: 40px;
+    margin: 0 5%;
+    font-size: larger;
+    background-color: white;
+    color: gray;
+    border: none;
+    border-radius: 10px;
   }
 </style>
