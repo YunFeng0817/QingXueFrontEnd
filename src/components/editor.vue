277aa986a2ae0307ba74490372e18a8432dfa480
Fitz <f18846188605@gmail.com>
2018-02-12 16:52:44 +0800

    为文本编辑器的js代码添加了一些注释

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -64,18 +64,25 @@
         } else {
           window.event.returnValue = false
         }
+        // 获取光标的range对象 event.view 是一个window对象
         let range = event.view.getSelection().getRangeAt(0);
+        // 光标的偏移位置
         let offset = range.startOffset;
+        // 新建一个span元素
         let span = document.createElement('span');
+        // 四个&nbsp表示四个空格
         span.innerHTML = '&nbsp&nbsp&nbsp&nbsp';
-
+        // 创建一个新的range对象
         let newrange = document.createRange();
+        // 设置新的range的位置，也是插入元素的位置
         newrange.setStart(range.startContainer, offset);
         newrange.setEnd(range.startContainer, offset);
         newrange.collapse(true);
         newrange.insertNode(span);
+        // 去掉旧的range对象，用新的range对象替换
         event.view.getSelection().removeAllRanges();
         event.view.getSelection().addRange(range);
+        // 将光标的位置向后移动一个偏移量，放到加入的四个空格后面
         range.setStart(span, 1);
         range.setEnd(span, 1);
       }
