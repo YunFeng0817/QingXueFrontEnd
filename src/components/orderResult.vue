d5833eb2074ed366710e5913363286ce06ee13d9
Fitz <f18846188605@gmail.com>
2018-02-28 12:56:08 +0800

    再次修改了订单详情界面删除评价的按钮样式

--- src/components/orderResult.vue
+++ src/components/orderResult.vue
@@ -64,7 +64,7 @@
     <div v-else-if="trade_status==='TRADE_SUCCESS'||trade_status==='TRADE_FINISHED'" class="order-body">
       <p>
         您的评价：
-        <a class="am-icon-remove" style="float:right;font-size:large;color:#e62739;margin: 0 0 0 20%;"
+        <a class="am-icon-remove" style="float:right;font-size:large;color:#e62739;margin: 0 0 0 8%;"
            @click="deleteComments(comment.id)"></a>
         <a class="am-icon-edit" style="float:right;font-size:large" @click="edit=true"></a>
       </p>
