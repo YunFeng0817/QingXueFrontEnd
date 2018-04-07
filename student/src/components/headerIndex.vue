3f62c6230cc7a32d25164636549f97a986264a74
Fitz <f18846188605@gmail.com>
2018-04-07 22:14:53 +0800

    修改了搜索的备选项

--- student/src/components/headerIndex.vue
+++ student/src/components/headerIndex.vue
@@ -53,8 +53,9 @@
       loadAll () {
         return [
           {'value': '高中'},
-          {'value': 'Hot honey 首尔炸鸡（仙霞路）', 'address': '上海市长宁区淞虹路661号'},
-          {'value': '新旺角茶餐厅', 'address': '上海市普陀区真北路988号创邑金沙谷6号楼113'}
+          {'value': '高中三年级'},
+          {'value': '初中四年级'},
+          {'value': '小学六年级'}
         ];
       },
       handleSelect (item) {
