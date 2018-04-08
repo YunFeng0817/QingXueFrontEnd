6ac46084717fb4e12d659054dd16adca26fa0443
Fitz <f18846188605@gmail.com>
2018-04-08 21:20:27 +0800

    更改了机构支付入驻金时，支付0元的情况处理

--- educator/src/components/pay.vue
+++ educator/src/components/pay.vue
@@ -37,39 +37,41 @@
           .then(function (response) {
             if (response.total_amount) {
               // 判断订单的金额如果大于0，正常支付
-              if (response.total_amount > 0) {
-                this.$confirm('<p style="max-width: 300px;width:80%;text-align: left;">您如果想要正常的发布课程，需要支付<span style="color:red">' + response.total_amount + ' </span>元 每年,</br/>如果选择取消，您可以正常登录管理后台，但是无法进行任何操作</p>', {
-                  confirmButtonText: '支付',
-                  cancelButtonText: '取消',
-                  center: true, // 文字是否居中
-                  lockScroll: false, // 在消息提示框显示时，是否允许滚动
-                  closeOnClickModal: false, // 是否可通过点击遮罩关闭 MessageBox
-                  closeOnPressEscape: false, // 是否可通过按下 ESC 键关闭 MessageBox
-                  dangerouslyUseHTMLString: true, // 是否将 message 属性作为 HTML 片段处理
-                  type: 'success'
-                }).then(() => {
-                  // 用户选择了支付的按钮
-                  window.location.href = response.payment_url;
-                }).catch(() => {
-                  // 用户选择了取消支付的按钮
-                  axios({
-                    url: '/api/order/registration_orders/?order_sn=' + response.order_sn,
-                    method: 'delete'
+              // if (response.total_amount > 0) {
+              this.$confirm('<p style="max-width: 300px;width:80%;text-align: left;">您如果想要正常的发布课程，需要支付<span style="color:red">' + response.total_amount + ' </span>元 每年,</br/>如果选择取消，您可以正常登录管理后台，但是无法进行任何操作</p>', {
+                confirmButtonText: '支付',
+                cancelButtonText: '取消',
+                center: true, // 文字是否居中
+                lockScroll: false, // 在消息提示框显示时，是否允许滚动
+                closeOnClickModal: false, // 是否可通过点击遮罩关闭 MessageBox
+                closeOnPressEscape: false, // 是否可通过按下 ESC 键关闭 MessageBox
+                dangerouslyUseHTMLString: true, // 是否将 message 属性作为 HTML 片段处理
+                type: 'success'
+              }).then(() => {
+                // 用户选择了支付的按钮
+                window.location.href = response.payment_url;
+              }).catch(() => {
+                // 用户选择了取消支付的按钮
+                axios({
+                  url: '/api/order/registration_orders/?order_sn=' + response.order_sn,
+                  method: 'delete'
+                })
+                  .then(function (response) {
                   })
-                    .then(function (response) {
-                    })
-                    .catch(function (error) {
-                      console.log(error);
-                    })
-                });
-              } else { // 订单的金额 <=0 ，提示不需要支付
-                this.$alert('您已获得免单机会，不需要支付入驻费用', {
-                  confirmButtonText: '确定',
-                  callback: action => {
-                    this.$router.replace({path: '/admin/'});
-                  }
-                });
-              }
+                  .catch(function (error) {
+                    console.log(error);
+                  })
+              });
+              // 下面注释的部分是在 支付为0元时给用户提示的处理方式，可能以后会用到
+              // }
+              // else { // 订单的金额 <=0 ，提示不需要支付
+              //   this.$alert('您已获得免单机会，不需要支付入驻费用', {
+              //     confirmButtonText: '确定',
+              //     callback: action => {
+              //       this.$router.replace({path: '/admin/'});
+              //     }
+              //   });
+              // }
             } else { // 用户未登录的情况
               this.$router.push({path: '/educator/login/pay'});
             }
