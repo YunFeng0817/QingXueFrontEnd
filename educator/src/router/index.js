5bcfcee63ff77ea8b42d4667d4d4e851d27e4f10
Fitz <f18846188605@gmail.com>
2018-04-14 19:23:31 +0800

    为educator部分添加了404的路由检查

--- educator/src/router/index.js
+++ educator/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import notFound from '../components/404';
 
 Vue.use(Router);
 
@@ -7,6 +8,10 @@ export default new Router({
   mode: 'history',
   linkActiveClass: 'router-link-active',
   routes: [
+    {
+      path: '*',
+      component: notFound
+    },
     {
       path: '/educator/login/:nextUrl?',
       component: resolve => require(['../components/signup'], resolve)
