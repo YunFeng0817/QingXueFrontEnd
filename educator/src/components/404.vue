b6a2f3ffd1031af545c20e8d39baf05b35e545c0
Fitz <f18846188605@gmail.com>
2018-04-14 19:20:21 +0800

    为educator部分添加了404页面

--- educator/src/components/404.vue
+++ educator/src/components/404.vue
@@ -3,18 +3,15 @@
     <h1>404 not found</h1>
     <h1>您找的页面不存在</h1>
     <h2>请您查看</h2>
-    <ol style=" list-style: none;">
+    <ol style=" list-style: none">
       <li>
-        <a href="/main">主页</a>
+        <a href="/educator/login">机构或教师账号登录</a>
       </li>
       <li>
-        <a href="/dynamic/normal/total">学情资讯</a>
+        <a href="/dynamic/signup">机构或教师账号注册</a>
       </li>
       <li>
-        <a href="/login">学生登录页面</a>
-      </li>
-      <li>
-        <a href="/educator/login">机构或教师登录页面</a>
+        <a href="/dynamic/pay">购买会员</a>
       </li>
     </ol>
   </div>
